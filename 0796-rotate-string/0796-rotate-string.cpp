aaa5cfcc60c295e634986da963e56d224bd06960
Lokesh Singh gosain <lokeshsinghgosain2318@gmail.com>
2026-09-02 22:48:10 +0530

    Time: 0 ms (100%), Space: 8 MB (83.18%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        // Strings must be same length to be rotations of each other
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        // Try all possible rotations of 's'
+        for (int i = 0; i < s.length(); i++) {
+            string rotated = s.substr(i) + s.substr(0, i);  
+            if (rotated == goal) {
+                return true;
+            }
+        }
+        return false;
+    
+
+
+        
+    }
+};
