d103a04a7fc4ecf19ad189d335c9b8da06fd4e40
Git Repo <lokeshsinghgosain2318@gmail.com>
2026-09-06 12:57:00 +0530

    Time: 7 ms (51.5%), Space: 13.9 MB (6.08%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int n=s.length();
+
+
+        map<char,int>mpp;
+
+        mpp['I']=1;
+        mpp['V']=5;
+        mpp['X']=10;
+        mpp['L']=50;
+        mpp['C']=100;
+        mpp['D']=500;
+        mpp['M']=1000;
+
+        int sum=0;
+
+    for(int i=0;i<n;i++){
+        if(mpp[s[i]]<mpp[s[i+1]]){
+            sum-=mpp[s[i]];
+
+        }
+        else{
+            sum+=mpp[s[i]];
+        }
+    }
+    return sum;
+    }
+    
+    
+        
+};
